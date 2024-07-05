5665b467ee413fd11ff0c930cb2ca4ed0b186b7b
kakadiayaprince <156084541+kakadiayaprince@users.noreply.github.com>
2024-07-05 09:17:30 +0530

    SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -297,18 +297,27 @@ CREATE TABLE STUDENT_DETAIL(
 ENROLLMENT_NO VARCHAR(20),
 NAME VARCHAR(25),
 CPI DECIMAL(5,2),
-BIRTHDATE DATETIME)
+BIRTHDATE DATETIME
+)
 
---1. Add two more columns City VARCHAR (20) (Not null) and Backlog INT (Null).ALTER TABLE STUDENT_DETAIL 
+--1. Add two more columns City VARCHAR (20) (Not null) and Backlog INT (Null).
+ALTER TABLE STUDENT_DETAIL 
 ADD CITY VARCHAR(20) NOT NULL,BACKLOG INT NULL
 
---2. Change the size of NAME column of student_detail from VARCHAR (25) to VARCHAR (35).ALTER  TABLE STUDENT_DETAIL 
+--2. Change the size of NAME column of student_detail from VARCHAR (25) to VARCHAR (35).
+ALTER  TABLE STUDENT_DETAIL 
 ALTER COLUMN  NAME  VARCHAR(25)
 
---3. Change the data type DECIMAL to INT in CPI Column.ALTER  TABLE STUDENT_DETAIL 
+--3. Change the data type DECIMAL to INT in CPI Column.
+ALTER  TABLE STUDENT_DETAIL 
 ALTER COLUMN  CPI  INT
 
---4. Rename Column Enrollment_No to ENO.EXEC SP_RENAME 'STUDENT_DETAIL.ENROLLMENT_NO', 'ENO'--5. Delete Column City from the student_detail table.ALTER TABLE STUDENT_DETAIL 
+--4. Rename Column Enrollment_No to ENO.
+EXEC SP_RENAME 'STUDENT_DETAIL.ENROLLMENT_NO', 'ENO'
+
+--5. Delete Column City from the student_detail table.
+ALTER TABLE STUDENT_DETAIL 
 DROP COLUMN CITY
 
---6. Change name of table student_detail to STUDENT_MASTER.EXEC SP_RENAME 'STUDENT_DETAIL', 'STUDENT_MASTER'
+--6. Change name of table student_detail to STUDENT_MASTER.
+EXEC SP_RENAME 'STUDENT_DETAIL', 'STUDENT_MASTER'
