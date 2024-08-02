597a5a8760284e0bd27e31251459d216cbaa5583
kakadiayaprince <156084541+kakadiayaprince@users.noreply.github.com>
2024-08-02 09:05:19 +0530

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -599,7 +599,256 @@ SELECT ENAME ,DEPARTMENT, MAX(SALARY) AS MAX_SAL FROM EMP
 GROUP BY ENAME,DEPARTMENT
 
 
-1. Count the number of employees living in Rajkot or Baroda.
-2. Display the total number of employees hired before 1st January, 1991 in IT department.
-3. Find the Joining Date wise Total Salaries.
-4. Find the Maximum salary department & city wise in which city name starts with ‘R’.
+--1. Count the number of employees living in Rajkot or Baroda.
+--2. Display the total number of employees hired before 1st January, 1991 in IT department.
+--3. Find the Joining Date wise Total Salaries.
+--4. Find the Maximum salary department & city wise in which city name starts with ‘R’.
+
+
+
+--LAB_8	
+
+CREATE TABLE SALES_DATA (
+REGION VARCHAR(25),
+PRODUCT VARCHAR(25),
+SALES_AMOUNT INT,
+YEAR INT
+)
+
+INSERT INTO SALES_DATA VALUES 
+('North America',' Watch' ,1500, 2023),
+('Europe', 'Mobile', 1200 ,2023),
+('Asia', 'Watch',1800 ,2023),
+('North America', 'TV', 900, 2024),
+('Europe', 'Watch', 2000,2024),
+('Asia' ,'Mobile', 1000, 2024),
+('North America', 'Mobile',1600, 2023),
+('Europe', 'TV', 1500, 2023),
+('Asia','TV', 1100, 2024),
+('North America' ,'Watch', 1700, 2024)
+
+
+--PART_A
+
+--1. Display Total Sales Amount by Region.
+SELECT REGION,SUM(SALES_AMOUNT) AS TOTAL_SALES FROM SALES_DATA GROUP BY REGION 
+
+--2. Display Average Sales Amount by Product
+SELECT PRODUCT,AVG(SALES_AMOUNT) AS AVARAGE_SALES FROM SALES_DATA GROUP BY PRODUCT
+
+--3. Display Maximum Sales Amount by Year
+SELECT YEAR,MAX(SALES_AMOUNT) AS MAXIMUM_SALES FROM SALES_DATA GROUP BY YEAR 
+
+--4. Display Minimum Sales Amount by Region and Year
+SELECT REGION,YEAR,MIN(SALES_AMOUNT) AS MINIMUM_SALES FROM SALES_DATA GROUP BY REGION,YEAR 
+
+--5. Count of Products Sold by Region
+SELECT REGION,COUNT(PRODUCT) AS COUNT_SALES FROM SALES_DATA GROUP BY REGION 
+
+--6. Display Sales Amount by Year and Product
+SELECT YEAR,PRODUCT, SUM(SALES_AMOUNT) AS TOTAL_SALES FROM SALES_DATA GROUP BY YEAR,PRODUCT
+
+--7. Display Regions with Total Sales Greater Than 5000
+SELECT REGION,SUM(SALES_AMOUNT) AS TOTAL_SALES FROM SALES_DATA GROUP BY REGION HAVING SUM(SALES_AMOUNT)>5000
+
+--8. Display Products with Average Sales Less Than 10000
+ SELECT PRODUCT,AVG(SALES_AMOUNT) AS AVARAGE_SALES FROM SALES_DATA GROUP BY PRODUCT HAVING AVG(SALES_AMOUNT)<10000
+
+--9. Display Years with Maximum Sales Exceeding 500
+SELECT YEAR,MAX(SALES_AMOUNT) AS MAXIMUM_SALES FROM SALES_DATA GROUP BY YEAR HAVING MAX(SALES_AMOUNT)>500
+
+--10. Display Regions with at Least 3 Distinct Products Sold.
+SELECT REGION,COUNT(DISTINCT PRODUCT)  AS MINIMUM_THREE_PRODUCTS FROM SALES_DATA GROUP BY REGION HAVING COUNT(DISTINCT PRODUCT)>=3
+
+--11. Display Years with Minimum Sales Less Than 1000
+SELECT YEAR,MIN(SALES_AMOUNT) AS MINIMUM_SALES FROM SALES_DATA GROUP BY YEAR HAVING MIN(SALES_AMOUNT)<1000
+
+--12. Display Total Sales Amount by Region for Year 2023, Sorted by Total Amount
+SELECT REGION,SUM(SALES_AMOUNT) AS TOTAL_SALES FROM SALES_DATA  WHERE YEAR=2023 GROUP BY REGION 
+
+
+--PART_B
+
+
+--1. Display Count of Orders by Year and Region, Sorted by Year and Region
+SELECT YEAR,REGION , COUNT(PRODUCT) AS COUNT_OF_PRODUCT FROM SALES_DATA GROUP BY YEAR,REGION ORDER BY YEAR,REGION
+--2. Display Regions with Maximum Sales Amount Exceeding 1000 in Any Year, Sorted by Region
+SELECT REGION , MAX(SALES_AMOUNT) AS TOTAL_SALES FROM SALES_DATA GROUP BY REGION HAVING MAX(SALES_AMOUNT) > 1000   ORDER BY REGION
+
+--3. Display Years with Total Sales Amount Less Than 1000, Sorted by Year Descending
+SELECT YEAR , SUM(SALES_AMOUNT)  AS TOTAL_SALES FROM SALES_DATA GROUP BY YEAR HAVING SUM(SALES_AMOUNT) < 1000   ORDER BY YEAR DESC
+
+--4. Display Top 3 Regions by Total Sales Amount in Year 2024
+SELECT TOP 3 REGION ,  SUM(SALES_AMOUNT)  AS TOTAL_SALES FROM SALES_DATA WHERE YEAR=2024 GROUP BY REGION 
+
+
+
+--PART_C
+
+
+--1. Display Products with Average Sales Amount Between 1000 and 2000, Ordered by Product Name
+SELECT PRODUCT , AVG(SALES_AMOUNT) AS AVARAGE_SALES FROM SALES_DATA WHERE SALES_AMOUNT BETWEEN 1000 AND 2000 GROUP BY PRODUCT  ORDER BY PRODUCT
+
+--2. Display Years with More Than 5 Orders from Each Region
+SELECT YEAR ,COUNT(PRODUCT) AS SALES FROM SALES_DATA WHERE COUNT(PRODUCT)>5  GROUP BY YEAR 
+
+--3. Display Regions with Average Sales Amount Above 1500 in Year 2023 sort by amount in descending.
+SELECT REGION , AVG(SALES_AMOUNT) AS AVARAGE_SALES FROM SALES_DATA WHERE AVG(SALES_AMOUNT)>1500 AND YEAR=2023 GROUP BY REGION ORDER BY AVG(SALES_AMOUNT) DESC
+--4. Find out region wise duplicate product.
+--5. Find out region wise highest sales amount.
+
+
+
+--LAB_6
+
+--PART_A_OF_MATH_FUNCTION
+
+--1. Display the result of 5 multiply by 30.
+SELECT (5*30) AS MULTIPLACTION 
+
+--2. Find out the absolute value of -25, 25, -50 and 50.
+SELECT ABS(-25) AS ABSULATE_VALUE, ABS(25) AS ABSULATE_VALUE, ABS(-50) AS ABSULATE_VALUE, ABS(50) AS ABSULATE_VALUE 
+
+--3. Find smallest integer value that is greater than or equal to 25.2, 25.7 and -25.2.
+SELECT CEILING(25.2) AS CEILING_VALUE,  CEILING(25.7) AS CEILING_VALUE , CEILING(-25.2) AS CEILING_VALUE
+
+--4. Find largest integer value that is smaller than or equal to 25.2, 25.7 and -25.2.
+ SELECT FLOOR(25.2) AS FLOOR_VALUE ,FLOOR(25.7) AS FLOOR_VALUE ,FLOOR(-25.2) AS FLOOR_VALUE
+
+--5. Find out remainder of 5 divided 2 and 5 divided by 3.
+SELECT (5%2) AS REMINDER ,(5%3) AS REMINDER 
+
+--6. Find out value of 3 raised to 2nd power and 4 raised 3rd power.
+SELECT POWER(3,2) AS _3_raised_to_2nd_power , POWER(4,3) AS _4_raised_to_3rd_power
+
+--7. Find out the square root of 25, 30 and 50.
+SELECT SQRT(25) AS SQUARE_ROOT_25 , SQRT(30) AS SQUARE_ROOT_30 , SQRT(50) AS SQUARE_ROOT_50
+
+--8. Find out the square of 5, 15, and 25.
+SELECT SQUARE(5) AS SQUARE_OF_5 , SQUARE(15) AS SQUARE_OF_15 , SQUARE(25) AS SQUARE_OF_25
+
+--9. Find out the value of PI.
+SELECT PI() AS VALAUE_OF_PI
+
+--10. Find out round value of 157.732 for 2, 0 and -2 decimal points.
+SELECT ROUND(157.732,2) ROUND_OF_UP_TO_2_DECIMAL_POINT , ROUND(157.732,0) ROUND_OF_UP_TO_0_DECIMAL_POINT , ROUND(157.732,-2) ROUND_OF_UP_TO_NEGATIVE_2_DECIMAL_POINT
+
+--11. Find out exponential value of 2 and 3.
+SELECT EXP(2) AS EXPONENTIOAL_VALUE_OF_2 ,  EXP(3) AS EXPONENTIOAL_VALUE_OF_3
+
+--12. Find out logarithm having base e of 10 and 2.
+SELECT LOG(10) AS LOGARITHM_OF_10 , LOG(2) AS LOGARITHM_OF_2
+
+--13. Find out logarithm having base b having value 10 of 5 and 100.
+SELECT LOG10(10) AS LOGARITHM_OF_10 , LOG10(5) AS LOGARITHM_OF_5 , LOG10(100) AS LOGARITHM_OF_100
+
+--14. Find sine, cosine and tangent of 3.1415.
+SELECT SIN(3.1415) AS SINE_  , COS(3.1415) AS COSINE_  , TAN(3.1415) AS TANGENT_ 
+
+--15. Find sign of -25, 0 and 25.
+ SELECT SIGN(-25) SIGN_OF_NEGATIVE_OF_25 , SIGN(0) SIGN_OF_0 , SIGN(25) SIGN_OF_25
+
+--16. Generate random number using function.
+SELECT CEILING(RAND()*100)  AS RANDOM_NUMBER_BETWEEN_1_TO_100
+
+
+
+--PART_A_STRING_FUNCTION
+
+
+--1.Find the length of following. (I) NULL (II) � hello � (III) Blank
+SELECT LEN('NULL') AS LENGTH_OF_NULL , LEN('HELLO') AS LENGTH_OF_HELLO , LEN('') AS LENGTH_OF_;
+
+--2.Display your name in lower & upper case.
+SELECT LOWER('PRINCE') AS LOWER_CASE , UPPER('PRINCE') AS UPPER_CASE;
+
+--3.Display first three characters of your name.
+SELECT SUBSTRING('PRINCE' , 1 ,3);
+
+--4.Display 3rd to 10th character of your name.
+SELECT SUBSTRING('PRINCEKAKADIYA' ,3,10);
+
+--5.Write a query to convert �abc123efg� to �abcXYZefg� & �abcabcabc� to �ab5ab5ab5� using REPLACE.
+SELECT REPLACE('abc123efg' , '123' , 'XYZ'), REPLACE('abcabcabc' , 'C' , '5');
+
+--6.Write a query to display ASCII code for �a�,�A�,�z�,�Z�, 0, 9.
+SELECT ASCII('A') AS ASCII_OF_A ,ASCII('a') AS ASCII_OF_a ,ASCII('Z') AS ASCII_OF_Z ,ASCII('z') AS ASCII_OF_z ,ASCII('0') AS ASCII_OF_0, ASCII('9') AS ASCII_OF_9;
+
+--7.Write a query to display character based on number 97, 65,122,90,48,57.
+SELECT CHAR('97'), CHAR('65'), CHAR('122'), CHAR('90'), CHAR('48'), CHAR('57');
+
+--8.Write a query to remove spaces from left of a given string � hello world �.
+SELECT LTRIM(' HELLO WORLD ');
+
+--9.Write a query to remove spaces from right of a given string � hello world �.
+SELECT RTRIM(' HELLO WORLD ');
+
+--10.Write a query to display first 4 & Last 5 characters of �SQL Server�.
+SELECT LEFT('SQL SERVER',4) , RIGHT('SQL SERVER' , 5);
+
+--11.Write a query to convert a string �1234.56� to number (Use cast and convert function).
+SELECT CAST(1234.56 AS numeric);
+SELECT CONVERT(numeric , 1234.56);
+
+--12.Write a query to convert a float 10.58 to integer (Use cast and convert function).
+SELECT CAST(10.58 AS numeric);
+SELECT CONVERT(numeric , 10.58);
+
+--13.Put 10 space before your name using function.
+SELECT SPACE(10) + 'PRINCE';
+
+--14.Combine two strings using + sign as well as CONCAT ().
+SELECT 'PRINCE' + 'KAKADIYA';
+SELECT CONCAT('PRINCE' , 'KAKADIYA');
+
+--15.Find reverse of Darshan.
+SELECT REVERSE('DARSHAN');
+
+--16.Repeat your name 3 times.
+SELECT REPLICATE('PRINCE' , 3);
+
+--PART_A_OF_DATE_FUNCTION 
+--1.Write a query to display the current date & time. Label the column Today_Date.
+SELECT GETDATE() AS TODAY_DATE;
+
+--2.Write a query to find new date after 365 day with reference to today.
+SELECT DATEADD(D,365 , GETDATE()) AS New_Date_After_365_Days;
+
+--3.Display the current date in a format that appears as may 5 1994 12:00AM.
+SELECT FORMAT(GETDATE(), 'MMMM d yyyy  hh:mmtt') AS Formatted_Date;
+
+--4.Display the current date in a format that appears as 03 Jan 1995.
+SELECT FORMAT(GETDATE(), 'd MMMM yyyy') AS Formatted_Date;
+
+--5.Display the current date in a format that appears as Jan 04, 96.
+SELECT FORMAT(GETDATE(), 'MMMM dd , yy') AS Formatted_Date;
+
+--6.Write a query to find out total number of months between 31-Dec-08 and 31-Mar-09.
+SELECT DATEDIFF(MONTH , '31-DEC-08', '31-MAR-09') AS NO_OF_MONTHS;
+
+--7.Write a query to find out total number of years between 25-Jan-12 and 14-Sep-10.
+SELECT DATEDIFF(YEAR , '14-SEP-10','25-JAN-12') AS NO_OF_YEAR;
+
+--8.Write a query to find out total number of hours between 25-Jan-12 7:00 and 26-Jan-12 10:30.
+SELECT DATEDIFF(HOUR , '25-JAN-12 7:00' , '26-JAN-12 10:30') AS NO_OF_HOUR;
+
+--9.Write a query to extract Day, Month, Year from given date 12-May-16.
+SELECT DATENAME(DAY , '12-MAY-16') AS NO_OF_DAY;
+SELECT DATENAME(MONTH , '12-MAY-16') AS NO_OF_MONTH;
+SELECT DATENAME(YEAR , '12-MAY-16') AS NO_OF_YEAR;
+
+--10.Write a query that adds 5 years to current date.
+SELECT DATEADD(YEAR, 5 , GETDATE()) AS FIVE_YEARS_ADDED;
+
+--11.Write a query to subtract 2 months from current date.
+SELECT DATEDIFF(MONTH , 02 , GETDATE()) AS TWO_MONTH_SUBTRACTED;
+
+--12.Extract month from current date using datename () and datepart () function.
+SELECT DATENAME(DAY , GETDATE()) AS MONTH_;
+SELECT DATEPART(DAY , GETDATE()) AS MONTH_;
+
+--13.Write a query to find out last date of current month.
+SELECT EOMONTH(GETDATE()) AS Last_Date_of_Current_Month;
+
+--14.Calculate your age in years and months.
+SELECT DATEDIFF(YEAR , '2005-11-30' , GETDATE());
